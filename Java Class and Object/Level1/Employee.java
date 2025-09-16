710ae4f45611bf83e075cc8f4a6ee895a566e0ff
Akhil2326 <akhil.rai_cs23@gla.ac.in>
2025-09-16 12:39:31 +0530

    Create Employee.java

---
+++ Java Class and Object/Level1/Employee.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.oops.levelone;
+
+class Employee {
+    // non-static variables
+    String name;
+    int id;
+    double salary;
+
+    // Constructor
+    Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    // Method to display employee details
