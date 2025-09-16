500ba7f5952b73ff0d1929363c40090bec3476ea
Akhil2326 <akhil.rai_cs23@gla.ac.in>
2025-09-16 11:26:31 +0530

    Create Person.java

---
+++ Constructor/Level1/Person.java
@@ -0,0 +1,16 @@
+class Person {
+    String name;
+    int age;
+
+    // Parameterized constructor
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Copy constructor
+    Person(Person other) {
+        this.name = other.name;
+        this.age = other.age;
+    }
+}
