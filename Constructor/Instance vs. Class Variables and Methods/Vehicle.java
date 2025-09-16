35004c3e68613e5e0b83c8bc1baab34b57c64683
Akhil2326 <akhil.rai_cs23@gla.ac.in>
2025-09-16 11:36:15 +0530

    Create Vehicle.java

---
+++ Constructor/Instance vs. Class Variables and Methods/Vehicle.java
@@ -0,0 +1,21 @@
+class Vehicle {
+    String ownerName;
+    String vehicleType;
+
+    static double registrationFee = 100.0;
+
+    Vehicle(String ownerName, String vehicleType) {
+        this.ownerName = ownerName;
+        this.vehicleType = vehicleType;
+    }
+
+    void displayVehicleDetails() {
+        System.out.println("Owner Name: " + ownerName);
+        System.out.println("Vehicle Type: " + vehicleType);
+        System.out.println("Registration Fee: $" + registrationFee);
+    }
+
+    static void updateRegistrationFee(double newFee) {
+        registrationFee = newFee;
+    }
+}
