1dcb185635f3f38b7c657d75d8114f2e67c40ba1
Akhil2326 <akhil.rai_cs23@gla.ac.in>
2025-09-16 10:51:53 +0530

    Create Order.java

---
+++ Inheritance/Mulltilevel_Inheritance/Order.java
@@ -0,0 +1,53 @@
+class Order {
+    String orderId;
+    String orderDate;
+
+    Order(String orderId, String orderDate) {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+    }
+
+    String getOrderStatus() {
+        return "Order placed";
+    }
+}
+
+class ShippedOrder extends Order {
+    String trackingNumber;
+
+    ShippedOrder(String orderId, String orderDate, String trackingNumber) {
+        super(orderId, orderDate);
+        this.trackingNumber = trackingNumber;
+    }
+
+    @Override
+    String getOrderStatus() {
+        return "Order shipped. Tracking Number: " + trackingNumber;
+    }
+}
+
+class DeliveredOrder extends ShippedOrder {
+    String deliveryDate;
+
+    DeliveredOrder(String orderId, String orderDate, String trackingNumber, String deliveryDate) {
+        super(orderId, orderDate, trackingNumber);
+        this.deliveryDate = deliveryDate;
+    }
+
+    @Override
+    String getOrderStatus() {
+        return "Order delivered on " + deliveryDate;
+    }
+}
+
+public class OrderManagementSystem {
+    public static void main(String[] args) {
+        Order order = new Order("ORD001", "2025-09-10");
+        ShippedOrder shippedOrder = new ShippedOrder("ORD002", "2025-09-11", "TRK12345");
+        DeliveredOrder deliveredOrder = new DeliveredOrder("ORD003", "2025-09-12", "TRK67890", "2025-09-15");
+
+        System.out.println(order.getOrderStatus());
+        System.out.println(shippedOrder.getOrderStatus());
+        System.out.println(deliveredOrder.getOrderStatus());
+    }
+}
