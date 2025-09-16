08053d4bd6701729f6e0eb08565cf814c49143de
Akhil2326 <akhil.rai_cs23@gla.ac.in>
2025-09-16 11:12:10 +0530

    Create Vehicle.java

---
+++ Operator/Vehicle.java
@@ -0,0 +1,26 @@
+class Vehicle {
+    static double registrationFee = 150.0;
+
+    String ownerName;
+    String vehicleType;
+    final String registrationNumber;
+
+    Vehicle(String ownerName, String vehicleType, String registrationNumber) {
+        this.ownerName = ownerName;
+        this.vehicleType = vehicleType;
+        this.registrationNumber = registrationNumber;
+    }
+
+    static void updateRegistrationFee(double newFee) {
+        registrationFee = newFee;
+    }
+
+    void displayDetails() {
+        if (this instanceof Vehicle) {
+            System.out.println("Owner Name: " + ownerName);
+            System.out.println("Vehicle Type: " + vehicleType);
+            System.out.println("Registration Number: " + registrationNumber);
+            System.out.println("Registration Fee: $" + registrationFee);
+        }
+    }
+}
