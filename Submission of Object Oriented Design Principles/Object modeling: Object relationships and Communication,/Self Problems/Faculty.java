d3d9e1138682d3b0ba2979fe2e6cc31f87ff4cb0
Akhil2326 <akhil.rai_cs23@gla.ac.in>
2025-09-16 12:03:31 +0530

    Create Faculty.java

---
+++ Submission of Object Oriented Design Principles/Object modeling: Object relationships and Communication,/Self Problems/Faculty.java
@@ -0,0 +1,11 @@
+class Faculty {
+    String name;
+    String designation;
+
+    Faculty(String name, String designation) {
+        this.name = name;
+        this.designation = designation;
+    }
+
+    void displayFaculty() {
+        System.out.println("Faculty: " + n
